e9e76e2120fa3fe146d6ccc1143b6a50ec9ddd48
eugene <eugenekennyjensen@gmail.com>
2022-04-28 21:23:50 -0400

    working on char sheet

--- scripts/ftd.js
+++ scripts/ftd.js
@@ -1,3 +1,12 @@
 /**
  * the overarching js file which imports js relating to actors, items, and tokens FINISH HTML FIRST / TODO
+ * the init hook loads in the new character sheet for now, and that is all
  */
+
+import { PCSheet } from "./pc-sheet";
+
+Hooks.once("init", async function(){
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("core", PCSheet, { makeDefault: true });
+    console.log("ass");
+});
---
+++ scripts/pc-sheet.js
@@ -0,0 +1,8 @@
+/**
+ * This defines the logic for handling a pc-sheet.html from the templates folder
+ */
+
+ export class PCSheet extends ActorSheet {
+
+    
+ }
