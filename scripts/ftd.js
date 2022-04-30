6b042ec420a7087e19c20f476c7ce8a6f1cd8251
eugene <eugenekennyjensen@gmail.com>
2022-04-30 02:55:21 -0400

    minimal changes

--- scripts/ftd.js
+++ scripts/ftd.js
@@ -3,10 +3,31 @@
  * the init hook loads in the new character sheet for now, and that is all
  */
 
-import { PCSheet } from "./pc-sheet";
+// import { PCSheet } from "./pc-sheet";
 
 Hooks.once("init", async function(){
-    Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet("core", PCSheet, { makeDefault: true });
-    console.log("ass");
+    // Actors.unregisterSheet("core", ActorSheet);
+    // Actors.registerSheet("core", PCSheet, { makeDefault: true });
+
+    const template_file = "../templates/actor-sheet.html";
+    const template_data = { header: "Handlebars header text.",
+                        content: "<em>Handlebars</em> <i>content</i>.",
+                        footer: "Handlebars footer text."};
+    const rendered_html = await renderTemplate(template_file, template_data);
+    let d = new Dialog({
+        title: "MyDialogTitle",
+        content: rendered_html,
+        buttons: {
+            toggle: {
+                icon: '<i class="fas fa-check"></i>',
+                label: "Okay",
+                callback: () => console.log("Okay")
+            },
+        },
+        default: "toggle",
+        close: html => {
+            console.log(html);
+        },
+    });
+    d.render(true);
 });
