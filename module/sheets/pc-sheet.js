5c42b58076c76651012b2059c1c3f23215599db2
eugene <eugenekennyjensen@gmail.com>
2022-05-09 00:31:41 -0400

    poop

--- module/sheets/pc-sheet.js
+++ module/sheets/pc-sheet.js
@@ -2,7 +2,22 @@
  * This defines the logic for handling a pc-sheet.html from the templates folder
  */
 
- export class PCSheet extends ActorSheet {
+export class PCSheet extends ActorSheet {
 
-    
- }
+    get template() {
+        return `systems/ftd/templates/sheets/${this.actor.data.type}-sheet.html`;
+    }
+
+    /**
+     * This method overrides the getData() method inherited from ItemSheet to do all the same things that the inherited method does,
+     * but then it adds a config property to the object returned by the method.
+     * @returns 
+     */
+    getData() {
+        const data = super.getData();
+
+        data.config = CONFIG.ftd;
+
+        return data;
+    }
+}
